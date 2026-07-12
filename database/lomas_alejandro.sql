be929d617bacad3ba08e9664cbf5e7bfb01ad0e3
Gerardosantes <perezzg816@gmail.com>
2026-07-12 15:42:45 -0600

    modifique los tamaño de varchar

--- database/lomas_alejandro.sql
+++ database/lomas_alejandro.sql
@@ -58,8 +58,8 @@ INSERT INTO `categorias` (`id_categoria`, `nombre`, `descripcion`, `estado`) VAL
 DROP TABLE IF EXISTS `roles`;
 CREATE TABLE IF NOT EXISTS `roles` (
   `id_rol` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `descripcion` varchar(150) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `nombre` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descripcion` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id_rol`),
   UNIQUE KEY `nombre` (`nombre`)
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -82,13 +82,13 @@ DROP TABLE IF EXISTS `usuarios`;
 CREATE TABLE IF NOT EXISTS `usuarios` (
   `id_usuario` int NOT NULL AUTO_INCREMENT,
   `id_rol` int NOT NULL,
-  `nombre` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `apellido_paterno` varchar(80) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `apellido_materno` varchar(80) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `usuario` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nombre` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `apellido_paterno` varchar(25) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `apellido_materno` varchar(25) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `usuario` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contrasena` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
   `telefono` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `correo` varchar(120) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `correo` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `estado` tinyint(1) DEFAULT '1',
   `fecha_registro` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_usuario`),
@@ -100,7 +100,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id_usuario`, `id_rol`, `nombre`, `apellido_paterno`, `apellido_materno`, `usuario`, `password`, `telefono`, `correo`, `estado`, `fecha_registro`) VALUES
+INSERT INTO `usuarios` (`id_usuario`, `id_rol`, `nombre`, `apellido_paterno`, `apellido_materno`, `usuario`, `contrasena`, `telefono`, `correo`, `estado`, `fecha_registro`) VALUES
 (1, 1, 'Gerardo', 'Lomas', 'Alejandro', 'admin', 'admin123', NULL, NULL, 1, '2026-07-12 16:30:55');
 COMMIT;
 
@@ -113,15 +113,15 @@ CREATE TABLE proveedores (
 
     id_proveedor INT AUTO_INCREMENT PRIMARY KEY,
 
-    nombre VARCHAR(100) NOT NULL,
+    nombre VARCHAR(50) NOT NULL,
 
     telefono VARCHAR(20),
 
-    correo VARCHAR(100),
+    correo VARCHAR(40),
 
-    direccion VARCHAR(200),
+    direccion VARCHAR(80),
 
-    contacto VARCHAR(100),
+    contacto VARCHAR(50),
 
     estado BOOLEAN DEFAULT TRUE,
 
